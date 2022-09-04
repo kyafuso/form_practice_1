24dce8d0ed2aec38ff65c8ea51681575a1df1307
kyafuso <kazuya.yafuso.0825@gmail.com>
2022-09-04 13:00:19 +0900

    一括登録機能実装

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @form = Form::ProductCollection.new
+  end
+
+  def create
+    @form = Form::ProductCollection.new(product_collection_params)
+    if @form.save
+      redirect_to products_path, notice: "商品を登録しました"
+    else
+      flash.now[:alert] = "商品登録に失敗しました"
+      render :new
+    end
+
+  end
+
+  private
+
+  def product_collection_params
+    params.require(:form_product_collection)
+          .permit(products_attributes: [:code, :name, :price, :availability])
+  end
+
+end
